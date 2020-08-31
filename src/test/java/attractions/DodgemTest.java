987c6661562dfdcf38308a1afc46776b897ab172
Tomek Nawrocki <tnawrocki1801@gmail.com>
2020-08-31 15:50:37 +0100

    ticketed implementation tests added for dodgem

--- src/test/java/attractions/DodgemTest.java
+++ src/test/java/attractions/DodgemTest.java
@@ -2,6 +2,7 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
@@ -28,5 +29,22 @@ public class DodgemTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, dodgems.getVisitCount());
+    }
+
+    @Test
+    public void hasDefaultPrice() {
+        assertEquals(4.5, dodgems.defaultPrice(), 0.01);
+    }
+
+    @Test
+    public void chargesStandardPriceOver12() {
+        Visitor visitor = new Visitor(12, 120, 20);
+        assertEquals(4.5, dodgems.priceFor(visitor), 0.01);
+    }
+
+    @Test
+    public void chargesHalfPriceOver12() {
+        Visitor visitor = new Visitor(11, 120, 20);
+        assertEquals(2.25, dodgems.priceFor(visitor), 0.01);
     }
 }
