70812c53941bb409a216348727e7159cfa4f41c6
Tomek Nawrocki <tnawrocki1801@gmail.com>
2020-08-31 15:28:40 +0100

    roller coaster security tests pass

--- src/main/java/attractions/RollerCoaster.java
+++ src/main/java/attractions/RollerCoaster.java
@@ -1,8 +1,16 @@
 package attractions;
 
-public class RollerCoaster  extends Attraction {
+import behaviours.Security;
+import people.Visitor;
+
+public class RollerCoaster  extends Attraction implements Security {
 
     public RollerCoaster(String name, int rating) {
         super(name, rating);
+    }
+
+    @Override
+    public boolean isAllowedTo(Visitor visitor) {
+        return visitor.getAge() > 12 && visitor.getHeight() > 145;
     }
 }
