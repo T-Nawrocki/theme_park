f592b97ccd088490078f956d3f4a751cc690d597
Tomek Nawrocki <tnawrocki1801@gmail.com>
2020-08-31 15:33:41 +0100

    Attraction implements Reviewed

--- src/main/java/attractions/Attraction.java
+++ src/main/java/attractions/Attraction.java
@@ -1,6 +1,8 @@
 package attractions;
 
-public abstract class Attraction {
+import behaviours.Reviewed;
+
+public abstract class Attraction implements Reviewed {
     private String name;
     private int rating;
     private int visitCount;
@@ -11,10 +13,12 @@ public abstract class Attraction {
         this.visitCount = 0;
     }
 
+    @Override
     public String getName() {
         return name;
     }
 
+    @Override
     public int getRating() {
         return rating;
     }
