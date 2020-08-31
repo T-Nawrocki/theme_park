698a8adb053a5d9a6632fab294808d06f6e89820
Tomek Nawrocki <tnawrocki1801@gmail.com>
2020-08-31 15:52:15 +0100

    dodgems passes ticketed implementation tests

--- src/main/java/attractions/Dodgems.java
+++ src/main/java/attractions/Dodgems.java
@@ -1,8 +1,21 @@
 package attractions;
 
-public class Dodgems extends Attraction {
+import behaviours.Ticketed;
+import people.Visitor;
+
+public class Dodgems extends Attraction implements Ticketed {
 
     public Dodgems(String name, int rating) {
         super(name, rating);
+    }
+
+    @Override
+    public double defaultPrice() {
+        return 4.5;
+    }
+
+    @Override
+    public double priceFor(Visitor visitor) {
+        return visitor.getAge() < 12 ? defaultPrice() / 2 : defaultPrice();
     }
 }
